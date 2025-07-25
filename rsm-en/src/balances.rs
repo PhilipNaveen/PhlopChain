76295424df82b77bf51811dc2efdae4b8e8535c1
PhilipNaveenUVA <dxj2ut@virginia.edu>
2025-07-25 15:13:44 -0400

    Added new struct/implementation for balanaces and transfer capabilities

---
+++ rsm-en/src/balances.rs
@@ -0,0 +1,49 @@
+use std::collections::BTreeMap;
+
+
+
+pub struct Pallet {
+
+    balances: BTreeMap<String, u128> // String for key, u128 for unsigned for positive-only vals
+
+}
+
+impl Pallet {
+
+    pub fn new() -> Self {
+        
+        Self {
+
+            balances: BTreeMap::new()
+        }
+    }
+
+
+    pub fn set_balance(&mut self, who: &String, amount: u128){
+
+        self.balances.insert(who.clone(), amount);
+    }
+
+
+    pub fn get_balance(&mut self, who: &String) -> u128{
+        
+        *self.balances.get(who).unwrap_or(&0)
+    }
+
+    pub fn transfer(&mut self, sender: String, reciever: String, amount: u128) -> Result<(), &'static str>{
+        
+        let sender_balance: u128 = self.get_balance(&sender);
+        let reciever_balance: u128 = self.get_balance(&reciever);
+
+        let new_sender_balance: u128 = sender_balance.checked_sub(amount).ok_or("Insufficient sender balance")?;
+        let new_reciever_balance: u128 = reciever_balance.checked_add(amount).ok_or("Error adding balance")?;
+
+        self.set_balance(&sender, new_sender_balance);
+        self.set_balance(&reciever, new_reciever_balance);
+
+        Ok(())
+    }
+
+
+
+}
--- rsm-en/src/main.rs
+++ rsm-en/src/main.rs
@@ -1,3 +1,11 @@
+use crate::balances::Pallet;
+
+mod balances;
+
 fn main() {
-    println!("Hello, world!");
+
+
+ 
+
+    
 }
